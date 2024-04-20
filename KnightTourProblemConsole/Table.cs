30acc8eca1feb13debffe61ab9bfcf0bafdb99d8
Leonardo Albuquerque Gil <leonardo.gil@ndd.tech>
2024-04-19 22:01:21 -0300

    Add project files.

---
+++ KnightTourProblemConsole/Table.cs
@@ -0,0 +1,15 @@
+﻿namespace KnightTourProblemConsole
+{
+    internal class Table
+    {
+        private readonly int n;
+
+        public int[,] Posicoes { get; private set; }
+
+        public Table(int n)
+        {
+            this.n = n;
+            Posicoes = new int[this.n, this.n];
+        }
+    }
+}
